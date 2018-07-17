8005e54059c0684eaf9f8775abf5451c7ce821e7
MS-Harine <21700371@handong.edu>
2018-07-17 11:22:19 +0900

    Add MainActivity
    Main Activity 구현

--- app/src/main/java/com/ahnbcilab/tremorquantification/tremorquantification/MainActivity.kt
+++ app/src/main/java/com/ahnbcilab/tremorquantification/tremorquantification/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.ahnbcilab.tremorquantification.tremorquantification
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spiralTestBtn.setOnClickListener {
+            startActivity(Intent(this, spiralTestListActivity::class.java))
+        }
+
+        patientListBtn.setOnClickListener {
+            startActivity(Intent(this, patientListActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/ahnbcilab/tremorquantification/tremorquantification/spiralTestListActivity.kt
@@ -0,0 +1,12 @@
+package com.ahnbcilab.tremorquantification.tremorquantification
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class spiralTestListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spiral_test_list)
+    }
+}

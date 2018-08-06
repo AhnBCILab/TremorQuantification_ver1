f722e2e4322080329b4fa5d3c3f759e1c0695ff8
MS-Harine <21700371@handong.edu>
2018-08-06 21:49:39 +0900

    Make Database controller
    기존에 Listview에서 Data controller에서 저장되지 않던 부분을
    SQLite를 이용해 저장하는 방향으로 변경.
    이 과정에서 DataController가 삭제되고 이 역할을 DBController가 함.
    그에 따른 ListViewAdapter도 변경

--- app/src/main/java/com/ahnbcilab/tremorquantification/data/PatientData.kt
+++ app/src/main/java/com/ahnbcilab/tremorquantification/data/PatientData.kt
@@ -2,11 +2,7 @@ package com.ahnbcilab.tremorquantification.data
 
 import java.util.*
 
-data class PatientData(var name: String, var birth: Int) {
-
-    // When connect with database, ID property will be here.
-    // val ID = db.getID()
-
+data class PatientData(var name: String, var birth: Int, var description: String?) {
+    var id: Int = -1
     val age: Int = Calendar.getInstance().get(Calendar.YEAR) - birth + 1
-    var description: String = ""
 }
--- app/src/main/java/com/ahnbcilab/tremorquantification/tremorquantification/SpiralTestListActivity.kt
+++ app/src/main/java/com/ahnbcilab/tremorquantification/tremorquantification/SpiralTestListActivity.kt
@@ -12,125 +12,146 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.*
-import com.ahnbcilab.tremorquantification.controller.DataController
+import com.ahnbcilab.tremorquantification.controller.DBController
 import com.ahnbcilab.tremorquantification.data.PatientData
 import kotlinx.android.synthetic.main.activity_spiral_test_list.*
 import kotlinx.android.synthetic.main.add_patient_dialog.*
 
 class SpiralTestListActivity : AppCompatActivity() {
 
-    companion object {
-        lateinit var adapter: PatientAdapter
-    }
+    private lateinit var adapter: PatientAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spiral_test_list)
 
-        SpiralTestListActivity.adapter = PatientAdapter(this, DataController.getDataList())
-        patientList.adapter = SpiralTestListActivity.adapter
+        adapter = PatientAdapter(this)
+        patientList.adapter = adapter
         patientList.emptyView = empty
 
         addBtn.setOnClickListener {
             val dialog = CustomAddDialog(this)
             dialog.show()
         }
-    }
-}
 
-class PatientAdapter(context: Context, val listItem: ArrayList<PatientData>) : BaseAdapter() {
-    private val mInflator: LayoutInflater = LayoutInflater.from(context)
-
-    private inner class ViewHolder {
-        lateinit var name: TextView
-        lateinit var description: TextView
-    }
-
-    fun add(data: PatientData) {
-        DataController.addData(data)
-    }
-
-    override fun getCount(): Int {
-        return this.listItem.size
-    }
-
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-
-    override fun getItem(position: Int): Any {
-        return this.listItem[position]
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View
-        val holder: ViewHolder
-
-        if (convertView == null) {
-            view = mInflator.inflate(R.layout.patient_list_item, parent, false)
-
-            holder = ViewHolder()
-            holder.name = view.findViewById(R.id.patientName) as TextView
-            holder.description = view.findViewById(R.id.patientDescription) as TextView
-            view.tag = holder
+        patientList.setOnItemClickListener { parent, view, position, id ->
+            run {
+                val id = view.findViewById<TextView>(R.id.PatientId).text.toString().toInt()
+                val intent = Intent(this, WrittenConsentActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                intent.putExtra("patientId", id)
+                startActivity(intent)
+                finish()
+            }
         }
-        else {
-            view = convertView
-            holder = convertView.tag as ViewHolder
+    }
+
+    override fun onDestroy() {
+        adapter.db.closeDB()
+        super.onDestroy()
+    }
+
+    inner class PatientAdapter(context: Context) : BaseAdapter() {
+        private val mInflator: LayoutInflater = LayoutInflater.from(context)
+        val db = DBController.PatientDataDbHelper(context)
+        private val listItem: Array<PatientData> = db.select(null, false, null, null, DBController.PatientDataDB.COLUMN_PATIENT_NAME)
+
+        private inner class ViewHolder {
+            lateinit var id: TextView
+            lateinit var name: TextView
+            lateinit var description: TextView
         }
 
-        val patient = getItem(position) as PatientData
-        val name = holder.name
-        val description = holder.description
-
-        name.text = patient.name
-        description.text = patient.description
-
-        return view
-    }
-}
-
-private class CustomAddDialog(context: Context) : Dialog(context) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
-        setContentView(R.layout.add_patient_dialog)
-
-        addDialogCancel.setOnClickListener {
-            dismiss()
+        fun add(data: PatientData) {
+            db.insert(data)
         }
 
-        addDialogAdd.setOnClickListener {
-            when {
-                addDialogName.text.isBlank() -> addDialogNameLayout.error = "Enter the name"
-                addDialogBirth.text.isBlank() -> addDialogBirthLayout.error = "Enter the birth"
-                else -> {
-                    val newData = PatientData(addDialogName.text.toString(), addDialogBirth.text.toString().toInt())
-                    if (addDialogDescription.text.isNotBlank())
-                        newData.description = addDialogDescription.text.toString()
+        override fun getCount(): Int {
+            return this.listItem.size
+        }
 
-                    SpiralTestListActivity.adapter.add(newData)
-                    dismiss()
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
 
-                    val intent = Intent(context, WrittenConsentActivity::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-                    startActivity(context, intent, null)
+        override fun getItem(position: Int): Any {
+            return this.listItem[position]
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val view: View
+            val holder: ViewHolder
+
+            if (convertView == null) {
+                view = mInflator.inflate(R.layout.patient_list_item, parent, false)
+
+                holder = ViewHolder()
+                holder.name = view.findViewById(R.id.patientName) as TextView
+                holder.description = view.findViewById(R.id.patientDescription) as TextView
+                holder.id = view.findViewById(R.id.PatientId) as TextView
+                view.tag = holder
+            }
+            else {
+                view = convertView
+                holder = convertView.tag as ViewHolder
+            }
+
+            val patient = getItem(position) as PatientData
+            val name = holder.name
+            val description = holder.description
+            val id = holder.id
+
+            name.text = patient.name
+            description.text = patient.description
+            id.text = patient.id.toString()
+
+            return view
+        }
+    }
+
+    inner class CustomAddDialog(context: Context) : Dialog(context) {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            window.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+            setContentView(R.layout.add_patient_dialog)
+
+            addDialogCancel.setOnClickListener {
+                dismiss()
+            }
+
+            addDialogAdd.setOnClickListener {
+                when {
+                    addDialogName.text.isBlank() -> addDialogNameLayout.error = "Enter the name"
+                    addDialogBirth.text.isBlank() -> addDialogBirthLayout.error = "Enter the birth"
+                    else -> {
+                        val newData = PatientData(
+                                addDialogName.text.toString(),
+                                addDialogBirth.text.toString().toInt(),
+                                addDialogDescription.text.toString())
+
+                        adapter.add(newData)
+                        dismiss()
+
+                        val intent = Intent(context, WrittenConsentActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                        startActivity(context, intent, null)
+                    }
                 }
             }
-        }
 
-        addDialogName.setOnFocusChangeListener { _, hasFocus ->
-            run {
-                if (!hasFocus && !addDialogName.text.isBlank())
-                    addDialogName.error = null
+            addDialogName.setOnFocusChangeListener { _, hasFocus ->
+                run {
+                    if (!hasFocus && !addDialogName.text.isBlank())
+                        addDialogName.error = null
+                }
             }
-        }
 
-        addDialogBirth.setOnFocusChangeListener { _, hasFocus ->
-            run {
-                if (!hasFocus && !addDialogBirth.text.isBlank())
-                    addDialogName.error = null
+            addDialogBirth.setOnFocusChangeListener { _, hasFocus ->
+                run {
+                    if (!hasFocus && !addDialogBirth.text.isBlank())
+                        addDialogName.error = null
+                }
             }
         }
     }

b44459a337f881e7eb84880a8e4280e14fcf7518
MS-Harine <21700371@handong.edu>
2018-09-02 18:47:50 +0900

    Change debug settings and the way to get raw data
    모바일 기기를 연결하여 디버깅을 할 수 있도록 앱 설정 변경
    기존에 Raw 데이터를 가져오는 방식을 변경. (60Hz 고정)

--- app/src/main/java/com/ahnbcilab/tremorquantification/controller/DBController.kt
+++ app/src/main/java/com/ahnbcilab/tremorquantification/controller/DBController.kt
@@ -29,7 +29,7 @@ object DBController {
 
     class PatientDataDbHelper(val context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
         companion object {
-            const val DATABASE_VERSION = 1
+            const val DATABASE_VERSION = 2
             const val DATABASE_NAME = "PatientData.db"
         }
 
